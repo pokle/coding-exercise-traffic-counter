b5344a641acf9bdd4c9afc35ea9b1f712ec1dc2a
Tushar Pokle <tushar.pokle@gmail.com>
2018-05-27 23:24:38 +1000

    lvs

--- group-by-date.py
+++ group-by-date.py
@@ -2,7 +2,8 @@ from collections import namedtuple
 from itertools import groupby
 from functools import reduce, partial
 
-Rec = namedtuple('Rec', ['datetime', 'cars'])
+# All our reducers work on a labeled values
+LV = namedtuple('LV', ['label', 'value'])
 
 
 def readfile(filename):
@@ -11,49 +12,46 @@ def readfile(filename):
 
 def parse(line):
     """
-    Parses a data line into the tuple (datetime:string, count:int)
+    Parses a data line into an LV
 
     >>> parse('2016-12-01T15:30:00 11')
-    Rec(datetime='2016-12-01T15:30:00', cars=11)
+    LV(label='2016-12-01T15:30:00', value=11)
     """
-    (datetime, cars) = line.strip().split(' ')
-    return Rec(datetime, int(cars))
+    (label, value) = line.strip().split(' ')
+    return LV(label, int(value))
 
 
-def pickDate(rec):
-    return rec.datetime.split('T')[0]
+def pickDate(lv):
+    return lv.label.split('T')[0]
 
-def pickCars(rec):
-    return rec.cars
+def pickCars(lv):
+    return lv.value
 
-def reduce_to_sum_cars(accum, rec):
-    return accum + rec.cars
-
-def rank(max_ranks, accum, record):
+def rank(max_ranks, accum, lv):
     """
-    A reducer, that ranks all your records by `max_ranks` positions.
+    A reducer, that ranks all your LVs by `max_ranks` positions.
 
     Single value:
-    >>> rank(3, {}, Rec('first', 1))
-    [Rec(datetime='first', cars=1)]
+    >>> rank(3, {}, LV('first', 1))
+    [LV(label='first', value=1)]
 
     Top 3 in the right order
-    >>> recs = [Rec('1st', 1), Rec('2nd', 2), Rec('3rd', 3)]
-    >>> reduce(partial(rank, 3), recs, {})
-    [Rec(datetime='3rd', cars=3), Rec(datetime='2nd', cars=2), Rec(datetime='1st', cars=1)]
+    >>> lvs = [LV('1st', 1), LV('2nd', 2), LV('3rd', 3)]
+    >>> reduce(partial(rank, 3), lvs, {})
+    [LV(label='3rd', value=3), LV(label='2nd', value=2), LV(label='1st', value=1)]
 
     Insertion and clamping:
-    >>> recs = [Rec('1st', 10), Rec('2nd', 20), Rec('3rd', 30), Rec('4th', 25)]
-    >>> reduce(partial(rank, 3), recs, {})
-    [Rec(datetime='3rd', cars=30), Rec(datetime='4th', cars=25), Rec(datetime='2nd', cars=20)]
+    >>> lvs = [LV('1st', 10), LV('2nd', 20), LV('3rd', 30), LV('4th', 25)]
+    >>> reduce(partial(rank, 3), lvs, {})
+    [LV(label='3rd', value=30), LV(label='4th', value=25), LV(label='2nd', value=20)]
     """
     if len(accum) == 0:
-        return [record] 
+        return [lv] # Straight to the top
 
     # Search for an insertion point
     for index, leader in enumerate(accum):
-        if record.cars > leader.cars:
-            accum.insert(index, record)
+        if lv.value > leader.value:
+            accum.insert(index, lv)
             if len(accum) > max_ranks:
                 accum.pop()
             return accum
@@ -64,18 +62,18 @@ def rank(max_ranks, accum, record):
 
 if __name__ == '__main__':
     lines = readfile('data.file')
-    records = map(parse, lines)
-    dateGroups = groupby(records, key=pickDate)
+    LVs = map(parse, lines)
+    dateGroups = groupby(LVs, key=pickDate)
 
-    total_cars = 0
+    total_value = 0
     ranks = []
     rank3 = partial(rank, 3)
 
     for v, group in dateGroups:
         group = list(group)
         print(v, sum(map(pickCars, group)))
-        total_cars = reduce(reduce_to_sum_cars, group, total_cars)
+        total_value = reduce(lambda a,v:a+v, group, total_value)
         ranks = reduce(rank3, group, ranks)
     
-    print('total cars=', total_cars)
+    print('total value=', total_value)
     print('top 3 =', ranks)
